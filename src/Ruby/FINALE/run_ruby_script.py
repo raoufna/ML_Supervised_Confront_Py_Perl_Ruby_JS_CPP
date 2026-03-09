83e3200feaec1e6bcbfa129ad311424ede1de04a
raouf namane <rnamane17@gmail.com>
2026-03-09 10:45:30 +0100

    codecarbon std for interp. lang

--- src/Ruby/FINALE/run_ruby_script.py
+++ src/Ruby/FINALE/run_ruby_script.py
@@ -1,21 +1,50 @@
-from codecarbon import EmissionsTracker
 import subprocess
-import pandas as pd
 import sys
+
+#CHECK E INSTALL CODECARBON
+print("Checking required libraries for CodeCarbon...")
+
+bash_command = sys.executable +" -m pip install --upgrade pip"
+# Execute the bash command
+print("bash_command:\n", bash_command)
+result = subprocess.run(bash_command, shell=True, capture_output=True, text=True)
+
+# List of libraries to check/install
+libraries = ["codecarbon", "pandas"]
+
+for lib in libraries:
+    try:
+        __import__(lib)
+        print(f"{lib} is already installed.")
+    except ImportError:
+        print(f"{lib} not found. Installing...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
+
+# Print the output and errors (if any)
+print("output:\t", result.stdout)
+error = result.stderr
+if(error):
+    print("errors:\t", error)
+
+print("All required libraries are available.\n")
+print("Starting the program...\n")
+
+#PROGRAM STARTS HERE
+from codecarbon import EmissionsTracker
+import pandas as pd
 
 tracker = EmissionsTracker()
 tracker.start()
 
 try:
-    argomenti = ["ruby", "script.rb", "spain_cardiac_arrest"]
+    dataset_name = sys.argv[1] if len(sys.argv) > 1 else "neuroblastoma" # CHANGE DATASET HERE
+    argomenti = ["ruby", "script.rb", dataset_name]
     
     subprocess.run(argomenti, check=True)
 finally:
     tracker.stop()
-    # Carica il file
-    df = pd.read_csv("emissions.csv")
 
-    # Prendi l'ultimo valore della colonna energy_consumed
-    ultimo_kwh = df["energy_consumed"].iloc[-1]
+    df = pd.read_csv("emissions.csv") # READ FILE
+    ultimo_kwh = df["energy_consumed"].iloc[-1] # GET the last energy_consumed value
 
-    print(f"energia consumata: {ultimo_kwh} kWh")
+    print(f"ENERGY CONSUMED: {ultimo_kwh} kWh")
