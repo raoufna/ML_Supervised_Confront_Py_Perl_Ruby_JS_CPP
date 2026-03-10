c621bfc42aaa820cff7af56caec8a2bc575d6028
raouf namane <rnamane17@gmail.com>
2026-03-10 11:28:15 +0100

    eng comment C++

--- src/C++/FINALE_WIN/main.cpp
+++ src/C++/FINALE_WIN/main.cpp
@@ -3,10 +3,10 @@
 int main(int argc, char* argv[]) {
     //START
     std::string dataset_name = (argc > 1)? argv[1] : neuroblastoma;    
-    arma::mat data = LoadData(relative_path + dataset_name + ".csv"); // carica il dataset nella matrice data
+    arma::mat data = LoadData(relative_path + dataset_name + ".csv"); // Upload the dataset in data
 
-    arma::rowvec Y_values = data.row(data.n_rows-1);  // salva il vettore soluzione(y)
-    data.shed_row(data.n_rows-1); // rimuove l'ultima riga(y) da data
+    arma::rowvec Y_values = data.row(data.n_rows-1);  // Save the solution vector(y)
+    data.shed_row(data.n_rows-1); // Remove the last row(y) from data
     
     arma::rowvec y_pred = LeaveOneOutCV(data, Y_values); //LEAVE-ONE-OUT CROSS-VALIDATION (LOOCV)
     double_t finalMCC = MCCEvaluator::Calculate(y_pred, Y_values); //CALCOLO MCC  
@@ -14,5 +14,5 @@ int main(int argc, char* argv[]) {
     //TIME IS PRINTED OUTSIDE
 
     return 0;
-    //FINE PROGRAMMA
+    //END PROGRAM
 }
